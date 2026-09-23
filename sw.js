e5d13efdb8502a16f8b8845fe30d8f90c31d73ce
dearducati8 <dearducati8@gmail.com>
2026-09-23 08:44:43 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,52 @@
-const CACHE='money-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'money-v2';
+
+const FILES = [
+'./',
+'./index.html',
+'./manifest.json'
+];
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(CACHE).then(cache => {
+return cache.addAll(FILES);
+})
+);
+
+self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys
+.filter(key => key !== CACHE)
+.map(key => caches.delete(key))
+);
+})
+);
+
+self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+fetch(event.request)
+.then(response => {
+const responseClone = response.clone();
+
+```
+    caches.open(CACHE).then(cache => {
+      cache.put(event.request, responseClone);
+    });
+
+    return response;
+  })
+  .catch(() => {
+    return caches.match(event.request);
+  })
+```
+
+);
+});
